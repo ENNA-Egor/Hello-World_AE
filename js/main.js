4f9763fcab7a1dc187fb1f964d8fbf4bfa7bce0b
ENNA-Egor <e.yudakov@gmail.com>
2022-12-25 22:32:50 +0300

    Первый коммит

--- js/main.js
+++ js/main.js
@@ -48,7 +48,21 @@
     }
     
         
-    init();
+  //  init();
 
 }());
     
+const filterBox = document.querySelectorAll('.box');
+
+
+document.querySelector('nav').addEventListener('click', Event=>{
+    if (Event.target.tagName !=='LI')return false;
+
+    let filterClass = Event.target.dataset['f'];
+    filterBox.forEach(elem =>{
+        elem.classList.remove('hide')
+        if (!elem.classList.contains(filterClass) && filterClass!=='all'){
+            elem.classList.add('hide');
+        }
+    });
+});
